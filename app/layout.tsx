2cacd9125d25144a2ac255095e9884d885b470c4
ayrtoncatri <ayrtoncatri@gmail.com>
2025-08-08 03:57:52 -0400

    Se agrega autenticacion y se cambia logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,26 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 
 export const metadata: Metadata = {
   title: "Beatbox Chile",
   description: "Comunidad oficial de Beatbox en Chile",
+  icons:{
+    icon: '/ISOTIPO-DEGRADADO.png'
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
       <body className="bg-gradient-to-b from-black via-blue-950 to-neutral-900 min-h-screen flex flex-col">
-        <Header />
-        <main>{children}</main>
-        <Footer />
+        <AuthProvider>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
