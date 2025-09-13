fdc7476d49641b1c87432f238be744b4fff26fab
ayrtoncatri <ayrtoncatri@gmail.com>
2025-09-13 10:04:35 -0300

    Se agrega a ruta de wilcard componente de listado de wildcard

--- app/wildcard/page.tsx
+++ app/wildcard/page.tsx
@@ -1,11 +1,13 @@
 import ReglasWildcard from "@/components/wildcards/ReglasWildcard";
 import FormularioWildcard from "@/components/wildcards/FormularioWildcard";
+import ListaWildcards from "@/components/wildcards/ListaWildcards";
 
 export default function WildcardPage() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-black via-blue-950 to-neutral-900 py-8 px-4">
       <ReglasWildcard />
       <FormularioWildcard />
+      <ListaWildcards />
     </main>
   );
 }
